e360dd86a2a7a84e911d33116dca9c5ff9d7ab2a
exploitx632 <exploitx632hql@gmail.com>
2025-11-14 00:32:50 +0800

    leap year

--- module-3/script.js
+++ module-3/script.js
@@ -1,18 +1,29 @@
-// convert inch to feet , miles to kilometer 
+// // convert inch to feet , miles to kilometer
 
-function inchToFeet (inch) {
-    const feet = inch / 12;
-    return feet;
-}
+// function inchToFeet (inch) {
+//     const feet = inch / 12;
+//     return feet;
+// }
 
-const height = inchToFeet(73);
-console.log(height);
+// const height = inchToFeet(73);
+// console.log(height);
 
+// function mileToKilometer (mile) {
+//     const kilometer =  1.609344 * mile;
+//     return kilometer;
+// }
 
-function mileToKilometer (mile) {
-    const kilometer =  1.609344 * mile;
-    return kilometer;
-}
+// const kilo = mileToKilometer(10);
+// console.log(kilo)
 
-const kilo = mileToKilometer(10);
-console.log(kilo)
+// // leap year
+
+// function checkLeapYear(year) {
+//   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+//     return `${year} is leap year`;
+//   }
+// }
+
+// const year = 2040;
+// const leapYear = checkLeapYear(year);
+// console.log(leapYear);
